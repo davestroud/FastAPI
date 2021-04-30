ce1bf15d32ba062273fc1c6fad549e1442d1fba0
davestroud <david@davidstroud.me>
2021-04-30 10:29:31 -0500

    create a base route in backend

---
+++ fastapi-react/backend/app/api.py
@@ -0,0 +1,11 @@
+# Before starting the server via the entry point,
+# create a base route
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
