249d9a3c22bce4215e081876a00d09c72fe8bba2
davestroud <david@davidstroud.me>
2021-04-30 13:25:25 -0500

    add frontend

--- fastapi-react/backend/app/api.py
+++ fastapi-react/backend/app/api.py
@@ -9,3 +9,15 @@ origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to your todo list."}
